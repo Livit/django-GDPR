0d61fe68020212ab4e499005543009108166d9b1
fredmajor <major.freddy@yahoo.com>
2020-10-19 18:49:45 +0100

    remove nested anonymizers

--- gdpr/anonymizers/__init__.py
+++ gdpr/anonymizers/__init__.py
@@ -1,11 +1,9 @@
 from __future__ import absolute_import
 
-from .generic_relation import GenericRelationAnonymizer, ReverseGenericRelationAnonymizer
 from .hash_fields import HashTextFieldAnonymizer, MD5TextFieldAnonymizer, SHA256TextFieldAnonymizer
 from .model_anonymizers import DeleteModelAnonymizer, ModelAnonymizer
 
 __all__ = (
     u'ModelAnonymizer', u'DeleteModelAnonymizer', u'MD5TextFieldAnonymizer',
-    u'ReverseGenericRelationAnonymizer', u'SHA256TextFieldAnonymizer',
-    u'HashTextFieldAnonymizer', u'GenericRelationAnonymizer',
+    u'SHA256TextFieldAnonymizer', u'HashTextFieldAnonymizer',
 )
